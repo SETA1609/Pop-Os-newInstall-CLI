c994ccc7642df43b27e48ee2687751aebcb3747f
seta1609 <sebastian.stp16@gmail.com>
2023-09-15 07:39:53 +0200

    fr: java + snapEnabler.sh were implemented

--- installer/functions/javaInstaller.sh
+++ installer/functions/javaInstaller.sh
@@ -4,6 +4,6 @@ log=/installer/logs/Java.log
 source checkExit.sh
 
 installJava(){
-  sudo apt apt install default-jdk 1>>"$log" 2>>"$errorLog"
+  sudo apt install default-jdk default-jre 1>>"$log" 2>>"$errorLog"
   checkExitStatus
 }
--- installer/functions/snapEnabler.sh
+++ installer/functions/snapEnabler.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+log=/installer/logs/Snap.log
+source checkExit.sh
+
+enableSnap(){
+  sudo apt install snapd 1>>"$log" 2>>"$errorLog"
+  checkExitStatus
+}
