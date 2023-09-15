9f3b9db8ce137d064f073f57e892bb5d96eb6757
seta1609 <sebastian.stp16@gmail.com>
2023-09-15 20:41:41 +0200

    ft: patchNvidea was implemented

--- resources/functions/getDefaultInstall.sh
+++ resources/functions/getDefaultInstall.sh
@@ -6,7 +6,7 @@ source /resources/functions/importSources.sh
 getSources
 
 # Define an array of function names
-functions=("getUpdate" "getSnap" "getVim" "getCurl" "getGrub" "getPython" "getJava" "getHaskell" "getNode" "getIntellij" "getDocker" "getDiscord" "getDeja" "patchNvidea")
+functions=("getUpdate" "getSnap" "getVim" "getCurl" "getGrub" "getPython" "getJava" "getHaskell" "getNode" "getIntellij" "getDocker" "getDiscord" "getDeja" )
 
 # Function to execute all functions in the array
 executeFunctions() {
--- resources/functions/nvideaErrorPatcher.sh
+++ resources/functions/nvideaErrorPatcher.sh
@@ -4,8 +4,21 @@ log=/resources/logs/Nvidea.log
 errorLog=/resources/errors/Nvidea.err
 
 source checkExit.sh
-
+# in case that syslog goes crazy in ubuntu/pop os  bug with nvidea
 patchNvidea(){
-  sudo apt install vim 1>>"$log" 2>>"$errorLog"
+  sudo apt purge ~nnvidea 1>>"$log" 2>>"$errorLog"
   checkExitStatus $errorLog
+
+  sudo apt clean 1>>"$log" 2>>"$errorLog"
+  checkExitStatus $errorLog
+
+  sudo apt update 1>>"$log" 2>>"$errorLog"
+  checkExitStatus $errorLog
+
+  sudo apt install system76-driver-nvidia 1>>"$log" 2>>"$errorLog"
+  checkExitStatus $errorLog
+
+  sudo systemctl reboot 1>>"$log" 2>>"$errorLog"
+  checkExitStatus $errorLog
+
 }
